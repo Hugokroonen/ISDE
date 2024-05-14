aaacda5cd9d9f3a967fbd321418796d888d9b152
Hugo Kroonen <95935259+Hugokroonen@users.noreply.github.com>
2024-05-14 18:11:16 +0200

    Added questions and options

--- models.py
+++ models.py
@@ -1,16 +1,29 @@
 from pydantic import BaseModel
 from typing import Optional, Union, Any, List
 
-# Base Option class
+class QuestionType(BaseModel):
+    SLIDER = "slider"
+    SELECTBOX = "selectbox"
+    NUMBER = "number_input"
+    TEXT = "text_input"
+
 class Option(BaseModel):
     text: str
-    value: Optional[Union[str, int]] = None
-    help: Optional[str] = None
-    icon_path: Optional[str] = None
+    value: Any
 
-    def __eq__(self, other: Any):
-        try:
-            return self.text == other.text and self.value == other.value
-        except (ValueError, AttributeError):
-            return False
-        return False
+class Question(BaseModel):
+    id: str
+    question_text: str
+    help_text: str | None = None
+    question_type: QuestionType
+    options: list[Answer] | None = None
+
+class Answer(BaseModel):
+    question_id: str
+    value: Any
+
+
+    condition_not_met = Answer(
+    question_id="postcode",
+    value = "Jouw gemeente doet helaas nog niet mee. Dat betekent dat je nog geen gebruik kunt maken van de gratis subsidiehulp"
+)
